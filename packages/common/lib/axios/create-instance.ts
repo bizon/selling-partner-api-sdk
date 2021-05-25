a95345d5d8ab7a473971618dd02f6d4421af72a4
Vincent Mesquita <vincent.mesquita@bizon.solutions>
2021-05-25 11:15:29 +0200

    [common] Update the way to create axios instance

--- packages/common/lib/axios/create-instance.ts
+++ packages/common/lib/axios/create-instance.ts
@@ -1,44 +1,51 @@
 import axios from 'axios'
 import {aws4Interceptor} from 'aws4-axios'
 
+import {AccessToken, SecurityTokenService} from '@bizon-sp-api-sdk/aws-auth'
+
 import pkg from '../../package.json'
 
 export interface ClientConfiguration {
-	awsCredentials: {
-		accessKeyId: string;
-		secretAccessKey: string;
-		sessionToken: string;
-	};
+	sts: SecurityTokenService;
 	region: string;
-	accessToken: string;
+	accessToken: AccessToken;
 	userAgent?: string;
 }
 
 export function createAxiosInstance({
 	accessToken,
-	awsCredentials,
+	sts,
 	/* eslint-disable-next-line @typescript-eslint/restrict-template-expressions */
 	userAgent = `${pkg.name}/${pkg.version}`,
 	region
 }: ClientConfiguration) {
 	const instance = axios.create({
 		headers: {
-			'x-amz-access-token': accessToken,
 			'user-agent': userAgent
 		}
 	})
 
-	instance.interceptors.request.use(aws4Interceptor(
-		{
-			region,
-			service: 'execute-api'
-		},
-		{
-			accessKeyId: awsCredentials.accessKeyId,
-			secretAccessKey: awsCredentials.secretAccessKey,
-			sessionToken: awsCredentials.sessionToken
-		}
-	))
+	instance.interceptors.request.use(async config => {
+		config.headers['x-amz-access-token'] = await accessToken.get()
+
+		return config
+	})
+
+	instance.interceptors.request.use(async config => {
+		const credentials = await sts.getCredentials()
+
+		return aws4Interceptor(
+			{
+				region,
+				service: 'execute-api'
+			},
+			{
+				accessKeyId: credentials.AccessKeyId ?? '',
+				secretAccessKey: credentials.SecretAccessKey ?? '',
+				sessionToken: credentials.SessionToken ?? ''
+			}
+		)(config)
+	})
 
 	return instance
 }

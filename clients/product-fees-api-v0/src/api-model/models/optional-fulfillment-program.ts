0dac81618a81769dbe2d9b86534ca00e590594b6
Bertrand Marron <bertrand.marron@gmail.com>
2021-06-22 12:49:01 +0200

    chore(build): rewrite build using tsc and remove rollup

--- clients/product-fees-api-v0/src/api-model/models/optional-fulfillment-program.ts
+++ clients/product-fees-api-v0/src/api-model/models/optional-fulfillment-program.ts
@@ -15,7 +15,7 @@
 
 
 /**
- * An optional enrollment program for which to return the estimated fees when the offer is fulfilled by Amazon (IsAmazonFulfilled is set to true).
+ * An optional enrollment program to return the estimated fees when the offer is fulfilled by Amazon (IsAmazonFulfilled is set to true).
  * @export
  * @enum {string}
  */

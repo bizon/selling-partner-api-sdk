83dbda70ff50d1949ac2ead792fab6fde2b5b527
Mesquita Vincent <vincent.mesquita@bizon.solutions>
2021-08-16 20:00:55 +0200

    fix: correct parameter types for clients constructors (#90)

--- clients/product-type-definitions-api-2020-09-01/src/client.ts
+++ clients/product-type-definitions-api-2020-09-01/src/client.ts
@@ -20,7 +20,7 @@ export const RATE_LIMITS: RateLimit[] = [
   },
 ]
 
-export interface ClientParameters extends Omit<ClientConfiguration, 'rateLimits | onRetry'> {
+export interface ClientParameters extends Omit<ClientConfiguration, 'rateLimits' | 'onRetry'> {
   rateLimiting?: {
     retry: boolean;
     onRetry?: onRetry;

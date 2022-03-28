044bbe65b23ec5f6941fe41d30955e95f49d4b11
Bertrand Marron <bertrand.marron@gmail.com>
2022-03-28 15:23:04 +0200

    fix(codegen): update logger prefix

--- codegen/utils/logger.ts
+++ codegen/utils/logger.ts
@@ -18,7 +18,7 @@ export const logger = winston.createLogger({
 
     const color = colors[value % colors.length]
 
-    return `${color(info.packageName)} : ${info.message}`
+    return `${color(`@sp-api-sdk/${info.packageName}`)}: ${info.message}`
   }),
   transports: [new winston.transports.Console()],
 })

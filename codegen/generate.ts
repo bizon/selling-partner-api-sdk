ef18009d9f548c20d84ff027509a8316189b5ce4
Bertrand Marron <bertrand.marron@gmail.com>
2022-08-16 11:35:52 +0200

    ci(codegen): allow running specific code generators

--- codegen/generate.ts
+++ codegen/generate.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 
 import * as childProcess from 'child_process'
 import fs from 'fs/promises'
@@ -8,17 +8,36 @@ import {promisify} from 'util'
 import {generateClients} from './generate-clients'
 import {generateNotificationSchemas} from './generate-notification-schemas'
 
+type Generator = 'clients' | 'notifications'
+
 const exec = promisify(childProcess.exec)
+
+async function runCommand(command: string) {
+  console.info(`Running: ${command}`)
+  return exec(command)
+}
+
+const generators: Set<Generator> = new Set((process.argv[2]?.split(',') as Generator[]) ?? [])
+if (generators.size === 0) {
+  generators.add('clients')
+  generators.add('notifications')
+}
 
 ;(async () => {
   await fs.rm('selling-partner-api-models', {recursive: true, force: true})
-  await exec('git clone https://github.com/amzn/selling-partner-api-models')
+  await runCommand('git clone https://github.com/amzn/selling-partner-api-models')
 
-  console.info('Generating clients…')
-  await generateClients()
+  if (generators.has('clients')) {
+    console.info('Generating clients…')
+    await generateClients()
+    await runCommand('xo --fix clients')
+  }
 
-  console.info('Generating notification schemas…')
-  await generateNotificationSchemas()
+  if (generators.has('notifications')) {
+    console.info('Generating notification schemas…')
+    await generateNotificationSchemas()
+    await runCommand('xo --fix packages/notifications')
+  }
 
   await fs.rm('selling-partner-api-models', {recursive: true})
 })().catch((error) => {

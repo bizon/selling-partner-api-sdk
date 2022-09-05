40e86181b824f9d5b5a38ffa30e8d57383547f76
Bertrand Marron <bertrand.marron@gmail.com>
2022-09-05 14:32:10 +0200

    chore: fix lint issues and rewrite jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,22 @@
+import process from 'process'
+
 import type {Config} from '@jest/types'
 
-const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: ['clients/*/src/*.ts', 'packages/*/src/**/*.ts'],
-  coverageReporters: ['lcov', 'text-summary'],
-}
+export default async function setup(): Promise<Config.InitialOptions> {
+  // Delete sensitive environment variables
+  delete process.env.AWS_ACCESS_KEY_ID
+  delete process.env.AWS_SECRET_ACCESS_KEY
+  delete process.env.AWS_ROLE_ARN
+  delete process.env.AWS_ROLE_SESSION_NAME
+  delete process.env.LWA_CLIENT_SECRET
+  delete process.env.LWA_CLIENT_ID
 
-export default config
+  return {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['clients/*/src/*.ts', 'packages/*/src/**/*.ts'],
+    coverageReporters: ['lcov', 'text-summary'],
+  }
+}

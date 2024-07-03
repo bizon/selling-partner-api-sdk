03e145b61508e819306001b98ecded8abafa23dc
tusbar <166147+tusbar@users.noreply.github.com>
2024-07-03 00:11:40 +0000

    feat(clients): update models as of 2024-07-03

--- clients/amazon-warehousing-and-distribution-2024-05-09/src/client.ts
+++ clients/amazon-warehousing-and-distribution-2024-05-09/src/client.ts
@@ -2,7 +2,29 @@ import {type ClientConfiguration, createAxiosInstance, type RateLimit} from '@sp
 
 import {AwdApi, Configuration} from './api-model'
 
-export const clientRateLimits: RateLimit[] = []
+export const clientRateLimits: RateLimit[] = [
+  {
+    method: 'get',
+    // eslint-disable-next-line prefer-regex-literals
+    urlRegex: new RegExp('^/awd/2024-05-09/inboundShipments/[^/]*$'),
+    rate: 2,
+    burst: 2,
+  },
+  {
+    method: 'get',
+    // eslint-disable-next-line prefer-regex-literals
+    urlRegex: new RegExp('^/awd/2024-05-09/inboundShipments$'),
+    rate: 1,
+    burst: 1,
+  },
+  {
+    method: 'get',
+    // eslint-disable-next-line prefer-regex-literals
+    urlRegex: new RegExp('^/awd/2024-05-09/inventory$'),
+    rate: 2,
+    burst: 2,
+  },
+]
 
 export class AmazonWarehousingAndDistributionClient extends AwdApi {
   constructor(configuration: ClientConfiguration) {

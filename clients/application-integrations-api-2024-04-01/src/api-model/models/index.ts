62346b3b4846ddfbe0b32496c7080fce42ad7800
tusbar <166147+tusbar@users.noreply.github.com>
2024-10-01 00:14:54 +0000

    feat(clients): update models as of 2024-10-01

---
+++ clients/application-integrations-api-2024-04-01/src/api-model/models/index.ts
@@ -0,0 +1,6 @@
+export * from './create-notification-request';
+export * from './create-notification-response';
+export * from './delete-notifications-request';
+export * from './error-list';
+export * from './model-error';
+export * from './record-action-feedback-request';
